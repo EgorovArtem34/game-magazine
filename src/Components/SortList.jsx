5923d78bb045522ccd7a997ed3cd17cc05edff9f
Artem Egorov <artegtor@gmail.com>
2023-05-02 13:31:42 +0500

    update sorlist active NavLink

--- src/Components/SortList.jsx
+++ src/Components/SortList.jsx
@@ -6,7 +6,7 @@ import { AiFillStar, AiFillFire } from 'react-icons/ai';
 import { TbPlayerTrackNextFilled } from 'react-icons/tb';
 import { BsNintendoSwitch } from 'react-icons/bs';
 import { useDispatch } from 'react-redux';
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 import { actions, fetchGames } from '../slices/gamesSlice.js';
 
 const categories = {
@@ -33,108 +33,141 @@ const SortList = () => {
     <>
       <div className="sort-category">
         <span className="sort-header">Top</span>
-        <ul>
-          <li>
-            <Link to={`/sorted/${categories.best}`}>
-              <button type="submit" onClick={() => handleClick(categories.best)}>
-                <div className="btn-img-wrap">
-                  <FaTrophy className="btn-img" />
-                </div>
-                <span className="link-text">Best games</span>
-              </button>
-            </Link>
-          </li>
-          <li>
-            <Link to={`/sorted/${categories.worst}`}>
-              <button type="button" onClick={() => handleClick(categories.worst)}>
-                <div className="btn-img-wrap">
-                  <FaThumbsDown className="btn-img" />
-                </div>
-                <span className="link-text">Worst games</span>
-              </button>
-            </Link>
-          </li>
-        </ul>
+        <nav>
+          <ul>
+            <li>
+              <NavLink
+                to={`/sorted/${categories.best}`}
+                className={({ isActive }) => (isActive ? 'active' : '')}
+              >
+                <button type="submit" onClick={() => handleClick(categories.best)}>
+                  <div className="btn-img-wrap">
+                    <FaTrophy className="btn-img" />
+                  </div>
+                  <span className="link-text">Best games</span>
+                </button>
+              </NavLink>
+            </li>
+            <li>
+              <NavLink
+                to={`/sorted/${categories.worst}`}
+                className={({ isActive }) => (isActive ? 'active' : '')}
+              >
+                <button type="button" onClick={() => handleClick(categories.worst)}>
+                  <div className="btn-img-wrap">
+                    <FaThumbsDown className="btn-img" />
+                  </div>
+                  <span className="link-text">Worst games</span>
+                </button>
+              </NavLink>
+            </li>
+          </ul>
+        </nav>
       </div>
       <div className="sort-category">
         <span className="sort-header">New Releases</span>
-        <ul>
-          <li>
-            <Link to={`/sorted/${categories.lastMonth}`}>
-              <button type="button" onClick={() => handleClick(categories.lastMonth)}>
-                <div className="btn-img-wrap">
-                  <AiFillStar className="btn-img" />
-                </div>
-                <span className="link-text">Last 30 days</span>
-              </button>
-            </Link>
-          </li>
-          <li>
-            <Link to={`/sorted/${categories.thisWeek}`}>
-              <button type="button" onClick={() => handleClick(categories.thisWeek)}>
-                <div className="btn-img-wrap">
-                  <AiFillFire className="btn-img" />
-                </div>
-                <span className="link-text">This week</span>
-              </button>
-            </Link>
-          </li>
-          <li>
-            <Link to={`/sorted/${categories.nextMonth}`}>
-              <button type="button" onClick={() => handleClick(categories.nextMonth)}>
-                <div className="btn-img-wrap">
-                  <TbPlayerTrackNextFilled className="btn-img" />
-                </div>
-                <span className="link-text">Next month</span>
-              </button>
-            </Link>
-          </li>
-        </ul>
+        <nav>
+          <ul>
+            <li>
+              <NavLink
+                to={`/sorted/${categories.lastMonth}`}
+                className={({ isActive }) => (isActive ? 'active' : '')}
+              >
+                <button type="button" onClick={() => handleClick(categories.lastMonth)}>
+                  <div className="btn-img-wrap">
+                    <AiFillStar className="btn-img" />
+                  </div>
+                  <span className="link-text">Last 30 days</span>
+                </button>
+              </NavLink>
+            </li>
+            <li>
+              <NavLink
+                to={`/sorted/${categories.thisWeek}`}
+                className={({ isActive }) => (isActive ? 'active' : '')}
+              >
+                <button type="button" onClick={() => handleClick(categories.thisWeek)}>
+                  <div className="btn-img-wrap">
+                    <AiFillFire className="btn-img" />
+                  </div>
+                  <span className="link-text">This week</span>
+                </button>
+              </NavLink>
+            </li>
+            <li>
+              <NavLink
+                to={`/sorted/${categories.nextMonth}`}
+                className={({ isActive }) => (isActive ? 'active' : '')}
+              >
+                <button type="button" onClick={() => handleClick(categories.nextMonth)}>
+                  <div className="btn-img-wrap">
+                    <TbPlayerTrackNextFilled className="btn-img" />
+                  </div>
+                  <span className="link-text">Next month</span>
+                </button>
+              </NavLink>
+            </li>
+          </ul>
+        </nav>
       </div>
       <div className="sort-category">
         <span className="sort-header">Platforms</span>
-        <ul>
-          <li>
-            <Link to={`/sorted/${categories.pc}`}>
-              <button type="button" onClick={() => handleClick(categories.pc)}>
-                <div className="btn-img-wrap">
-                  <FiMonitor className="btn-img" />
-                </div>
-                <span className="link-text">PC</span>
-              </button>
-            </Link>
-          </li>
-          <li>
-            <Link to={`/sorted/${categories.playStation5}`}>
-              <button type="button" onClick={() => handleClick(categories.playStation5)}>
-                <div className="btn-img-wrap">
-                  <FaPlaystation className="btn-img" />
-                </div>
-                <span className="link-text">PlayStation 5</span>
-              </button>
-            </Link>
-          </li>
-          <li>
-            <Link to={`/sorted/${categories.xboxOne}`}>
-              <button type="button" onClick={() => handleClick(categories.xboxOne)}>
-                <div className="btn-img-wrap">
-                  <FaXbox className="btn-img" />
-                </div>
-                <span className="link-text">Xbox One</span>
-              </button>
-            </Link>
-          </li>
-          <li>
-            <Link to={`/sorted/${categories.nintendoSwitch}`}>
-              <button type="button" onClick={() => handleClick(categories.nintendoSwitch)}>
-                <div className="btn-img-wrap">
-                  <BsNintendoSwitch className="btn-img" />
-                </div>
-                <span className="link-text">Nintendo Switch</span>
-              </button>
-            </Link>
-          </li>
-        </ul>
+        <nav>
+          <ul>
+            <li>
+              <NavLink
+                to={`/sorted/${categories.pc}`}
+                className={({ isActive }) => (isActive ? 'active' : '')}
+              >
+                <button type="button" onClick={() => handleClick(categories.pc)}>
+                  <div className="btn-img-wrap">
+                    <FiMonitor className="btn-img" />
+                  </div>
+                  <span className="link-text">PC</span>
+                </button>
+              </NavLink>
+            </li>
+            <li>
+              <NavLink
+                to={`/sorted/${categories.playStation5}`}
+                className={({ isActive }) => (isActive ? 'active' : '')}
+              >
+                <button type="button" onClick={() => handleClick(categories.playStation5)}>
+                  <div className="btn-img-wrap">
+                    <FaPlaystation className="btn-img" />
+                  </div>
+                  <span className="link-text">PlayStation 5</span>
+                </button>
+              </NavLink>
+            </li>
+            <li>
+              <NavLink
+                to={`/sorted/${categories.xboxOne}`}
+                className={({ isActive }) => (isActive ? 'active' : '')}
+              >
+                <button type="button" onClick={() => handleClick(categories.xboxOne)}>
+                  <div className="btn-img-wrap">
+                    <FaXbox className="btn-img" />
+                  </div>
+                  <span className="link-text">Xbox One</span>
+                </button>
+              </NavLink>
+            </li>
+            <li>
+              <NavLink
+                to={`/sorted/${categories.nintendoSwitch}`}
+                className={({ isActive }) => (isActive ? 'active' : '')}
+              >
+                <button type="button" onClick={() => handleClick(categories.nintendoSwitch)}>
+                  <div className="btn-img-wrap">
+                    <BsNintendoSwitch className="btn-img" />
+                  </div>
+                  <span className="link-text">Nintendo Switch</span>
+                </button>
+              </NavLink>
+            </li>
+          </ul>
+        </nav>
       </div>
     </>
   );
