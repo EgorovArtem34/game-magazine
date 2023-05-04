5dac526e9e7ed954c9606d46e4fb63709a9f00e1
Artem Egorov <artegtor@gmail.com>
2023-05-04 12:59:32 +0400

    update src/init.jsx change browseRouter to HashRouter

--- src/init.jsx
+++ src/init.jsx
@@ -1,5 +1,5 @@
 import { Provider } from 'react-redux';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { HashRouter as Router } from 'react-router-dom';
 import App from './App';
 import store from './slices/index.js';
 
