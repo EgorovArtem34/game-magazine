11c0c7f7fd9d0df2ca261c4ea854a23001f0c9f9
Artem Egorov <artegtor@gmail.com>
2023-05-04 13:03:32 +0400

    add src/App.jsx add redirection

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, Navigate } from 'react-router-dom';
 import './App.css';
 import GameLists from './Components/GameLists';
 import SortList from './Components/SortList';
@@ -16,9 +16,10 @@ const App = () => (
         </div>
         <div className="content">
           <Routes>
-            <Route path={routes.defaultPath()} element={<GameLists />} />
+            <Route index element={<GameLists />} />
             <Route path={routes.sorted()} element={<GameLists />} />
             <Route path={routes.game()} element={<GameInfo />} />
+            <Route path="*" element={<Navigate to={routes.defaultPath()} />} />
           </Routes>
         </div>
       </div>
